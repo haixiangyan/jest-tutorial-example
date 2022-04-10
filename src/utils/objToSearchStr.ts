a2d5f170e3d185b3414df22726fde172fc4af455
HaixiangYan <haixiang6123@gmail.com>
2022-04-11 00:35:22 +0800

    feat: 使用 TDD 来完成 objToSearchStr

---
+++ src/utils/objToSearchStr.ts
@@ -0,0 +1,15 @@
+const objToSearchStr = (obj: Record<string, string | number>) => {
+  // ['a=1', 'b=2']
+  const strPairs: string[] = [];
+
+  Object.entries(obj).forEach((keyValue) => {
+    const [key, value] = keyValue; // [a, 1]
+    const pair = key + '=' + String(value); // a=1
+    strPairs.push(pair);
+  }, []);
+
+  // a=1&b=2
+  return strPairs.join('&');
+}
+
+export default objToSearchStr;
---
+++ tests/utils/objToSearchStr.test.ts
@@ -0,0 +1,10 @@
+import objToSearchStr from "utils/objToSearchStr";
+
+describe('objToSearchStr', () => {
+  it('可以将对象转化成查询参数字符串', () => {
+    expect(objToSearchStr({
+      a: '1',
+      b: '2',
+    })).toEqual('a=1&b=2');
+  })
+})
