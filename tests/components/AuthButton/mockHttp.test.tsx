6f8044f45024796205fbe00794871bfd72c20ea7
HaixiangYan <haixiang6123@gmail.com>
2022-04-11 00:44:14 +0800

    feat: 添加 setup 函数

--- tests/components/AuthButton/mockHttp.test.tsx
+++ tests/components/AuthButton/mockHttp.test.tsx
@@ -4,16 +4,22 @@ import {rest} from "msw";
 import {render, screen} from "@testing-library/react";
 import AuthButton from "components/AuthButton";
 import React from "react";
+import {UserRoleType} from "apis/user";
+
+// 初始化函数
+const setup = (userType: UserRoleType) => {
+  server.use(
+    rest.get('https://mysite.com/api/role', async (req, res, ctx) => {
+      return res(
+        ctx.status(200),
+        ctx.json({ userType }))
+    })
+  )
+};
 
 describe('AuthButton Mock Http 请求', () => {
   it('可以正确展示普通用户按钮内容', async () => {
-    server.use(
-      rest.get('https://mysite.com/api/role', async (req, res, ctx) => {
-        return res(
-          ctx.status(200),
-          ctx.json({ userType: 'user' }))
-      })
-    )
+    setup('user');
 
     render(<AuthButton>你好</AuthButton>)
 
@@ -21,13 +27,7 @@ describe('AuthButton Mock Http 请求', () => {
   })
 
   it('可以正确展示管理员按钮内容', async () => {
-    server.use(
-      rest.get('https://mysite.com/api/role', async (req, res, ctx) => {
-        return res.once(
-          ctx.status(200),
-          ctx.json({ userType: 'admin' }))
-      })
-    )
+    setup('admin');
 
     render(<AuthButton>你好</AuthButton>)
 
