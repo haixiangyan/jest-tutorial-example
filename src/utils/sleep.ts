15baf6746df84cc03d254b6f2405756c92b6ef19
HaixiangYan <haixiang6123@gmail.com>
2022-04-12 01:42:51 +0800

    feat: 添加 sleep 函数以及测试代码

---
+++ src/utils/sleep.ts
@@ -0,0 +1,5 @@
+const sleep = (ms: number) => {
+  return new Promise((resolve => setTimeout(resolve, ms)))
+}
+
+export default sleep;
---
+++ tests/mockServer/sleep.test.ts
@@ -0,0 +1,29 @@
+import sleep from "utils/sleep";
+
+describe('sleep', () => {
+  it('可以在 1s 后再执行',  async () => {
+    jest.useFakeTimers();
+
+    const act = async (callback: Function) => {
+      await sleep(1000)
+      callback();
+    }
+
+    const mockCallback = jest.fn(() => console.log('callback'));
+
+    const promise = act(mockCallback);
+
+    // mockCallback 还未调用
+    expect(mockCallback).not.toBeCalled();
+
+    // 执行 setTimeout 里的 resolve
+    jest.runAllTimers();
+
+    // 执行 callback 内容
+    await promise;
+
+    // mockCallback 已调用
+    expect(mockCallback).toBeCalled();
+    expect(mockCallback).toHaveBeenCalledTimes(1);
+  })
+});
