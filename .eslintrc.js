21b3199640e2cd321cd0e944cb144bfb0d76ef46
HaixiangYan <haixiang6123@gmail.com>
2022-04-13 00:42:46 +0800

    feat: 添加更多规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,16 +3,23 @@ module.exports = {
   env: {
     browser: true,
     node: true,
+    "jest/globals": true
   },
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint', 'jest'],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:jest/recommended',
   ],
   rules: {
     '@typescript-eslint/no-non-null-assertion': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-    'no-var': 'off'
+    'no-var': 'off',
+    "jest/no-disabled-tests": "warn",
+    "jest/no-focused-tests": "error",
+    "jest/no-identical-title": "error",
+    "jest/prefer-to-have-length": "warn",
+    "jest/valid-expect": "error"
   }
 }
