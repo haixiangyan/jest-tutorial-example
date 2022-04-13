ade86af25e7732d771fa24fde85862ca0b653f45
HaixiangYan <haixiang6123@gmail.com>
2022-04-13 13:10:57 +0800

    feat: 使用 @testing-library/react-hooks 来做测试，并锁死版本

---
+++ tests/hooks/useCounter/ReactTestingLibrary.test.tsx
@@ -0,0 +1,46 @@
+import {renderHook} from "@testing-library/react-hooks";
+import useCounter from "hooks/useCounter";
+import {act} from "@testing-library/react";
+
+describe('useCounter', () => {
+  it('可以做加法', () => {
+    const { result } = renderHook(() => useCounter(0));
+
+    act(() => {
+      result.current[1].inc(1);
+    })
+
+    expect(result.current[0]).toEqual(1);
+  });
+
+  it('可以做减法', () => {
+    const { result } = renderHook(() => useCounter(0));
+
+    act(() => {
+      result.current[1].dec(1);
+    })
+
+    expect(result.current[0]).toEqual(-1);
+  });
+
+  it('可以设置值', () => {
+    const { result } = renderHook(() => useCounter(0));
+
+    act(() => {
+      result.current[1].inc(10);
+    })
+
+    expect(result.current[0]).toEqual(10);
+  });
+
+  it('可以重置值', () => {
+    const { result } = renderHook(() => useCounter(0));
+
+    act(() => {
+      result.current[1].inc(1);
+      result.current[1].reset();
+    })
+
+    expect(result.current[0]).toEqual(0);
+  });
+});
