5e222e510f3ad7593b8a25adf36c06c4b40004d6
haixiangyan <Qq680321>
2022-04-13 15:52:09 +0800

    feat: 添加 eslint-plugin-testing-library

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
     "jest/globals": true
   },
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'jest'],
+  plugins: ['@typescript-eslint', 'jest', 'testing-library'],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
@@ -18,12 +18,16 @@ module.exports = {
     '@typescript-eslint/no-explicit-any': 'off',
     'no-var': 'off',
     '@typescript-eslint/no-var-requires': 'off',
+    "testing-library/no-dom-import": "off",
     // 错误提示
     "jest/no-focused-tests": "error",
     "jest/no-identical-title": "error",
     "jest/valid-expect": "error",
+    "testing-library/await-async-query": "error",
+    "testing-library/no-await-sync-query": "error",
     // 告警提示
     "jest/no-disabled-tests": "warn",
     "jest/prefer-to-have-length": "warn",
+    "testing-library/no-debugging-utils": "warn",
   }
 }
