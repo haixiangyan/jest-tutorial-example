fc7214b250f314afbac482102c824727646f6c8b
haixiangyan <Qq680321>
2022-04-13 15:53:24 +0800

    feat: 添加规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:jest/recommended',
+    'plugin:testing-library/react',
   ],
   rules: {
     // 关闭规则
